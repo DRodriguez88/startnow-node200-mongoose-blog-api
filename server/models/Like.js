e4fe3a62c86dd654d8f8a4942aa51bc30429366a
Dj <rodriguezdanilo88@gmail.com>
2018-04-12 18:35:07 -0700

    a user can now post a "like" relating to a "blog"

--- server/models/Like.js
+++ server/models/Like.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const LikeSchema = new Schema ({
-        likerId: { type: Schema.Types.ObjectId, required: true },
-        liker: { type: Schema.Types.ObjectId, ref: 'User' },
-        blogId: { type:Schema.Types.ObjectId, ref: 'Blog' } 
+        liker: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        blogId: { type:Schema.Types.ObjectId, ref: 'Blog', required: true }
 });
 
 module.exports = mongoose.model('Like', LikeSchema);
--- server/routes/likes.js
+++ server/routes/likes.js
@@ -4,6 +4,8 @@ const Blog = require('../models/Blog');
 const User = require('../models/User');
 const Comment = require('../models/Comment');
 const Like = require('../models/Like');
+let dbLiker = null;
+let dbLike = null;
 
 router.get('/', (req,res) => {
     Like
@@ -14,7 +16,27 @@ router.get('/', (req,res) => {
 });
 
 router.post('/', (req,res) => {
-    
-})
+    User
+        .findById(req.body.liker)
+        .then(user => {
+            dbLiker = user;
+            var like = new Like(req.body);
+            like.liker = user._id;
+            return like.save()
+        })
+        .then(like => {
+            dbLike = like;
+            dbLiker.likes.push(like);
+            dbLiker.save().then(() => res.status(201).json(like));
+        })
+        .then( () => {
+            Blog
+                .findById(req.body.blogId)
+                .then(blog => {
+                    blog.likes.push(dbLike);
+                    blog.save()
+                });
+        });
+});
 
 module.exports = router;
